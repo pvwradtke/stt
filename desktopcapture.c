06d3d5b551cf72d206a33a453b2310e06c396b0a
Paulo Vinicius Wolski Radtke <pvwradtke@gmail.com>
2011-01-20 09:41:57 -0200

    Remove unistd.h on webcamcapture.cpp (not required) and changed indentation.

--- desktopcapture.c
+++ desktopcapture.c
@@ -14,72 +14,62 @@ Copyright 2010-2011, Paulo Vinicius Wolski Radtke (pvwradtke@gmail.com)
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 
-int main(int argc, char **argv)
-{
-	int counter=0, interval=30, fontsize=40;
-	char text[200];
-	time_t rawtime;
-	struct tm * timeinfo;
-	int argpos = 1;
-	// Retrieves arguments and do a sanity check, just in case
-    while(argpos < argc)
-	{
-		if(strcmp(argv[argpos], "help")==0)
-		{
-            printf("Usage: desktopcapture [ OPTIONS ]\n"); 
-			printf("Where OPTIONS is a combination of one or more of the following:\n");
-			printf("--counter | -c : defaults to zero, indicates first frame counter\n");
-			printf("--interval | -i : defaults to 30 seconds, sets the interval  between frames\n");
-			printf("--fontsize | -f : defaults to 40, change accordingly to your desktop resolution\n");
-			printf("--help | -h : shows this message. Any other parameter is ignored.\n");
-			printf("Parameters may be passed in any given order. \n");
-			return 0;
-		}
-        else if(strcmp(argv[argpos], "--counter")==0 || strcmp(argv[argpos], "-c")==0)
-        {
-            counter = atoi(argv[argpos+1]);
-            if(counter < 0)
+int main(int argc, char **argv) {
+    int counter = 0, interval = 30, fontsize = 40;
+    char text[200];
+    time_t rawtime;
+    struct tm * timeinfo;
+    int argpos = 1;
+    // Retrieves arguments and do a sanity check, just in case
+    while (argpos < argc) {
+        if (strcmp(argv[argpos], "help") == 0) {
+            printf("Usage: desktopcapture [ OPTIONS ]\n");
+            printf("Where OPTIONS is a combination of one or more of the following:\n");
+            printf("--counter | -c : defaults to zero, indicates first frame counter\n");
+            printf("--interval | -i : defaults to 30 seconds, sets the interval  between frames\n");
+            printf("--fontsize | -f : defaults to 40, change accordingly to your desktop resolution\n");
+            printf("--help | -h : shows this message. Any other parameter is ignored.\n");
+            printf("Parameters may be passed in any given order. \n");
+            return 0;
+        } else if (strcmp(argv[argpos], "--counter") == 0 || strcmp(argv[argpos], "-c") == 0) {
+            counter = atoi(argv[argpos + 1]);
+            if (counter < 0)
                 counter = 0;
-            argpos+=2;
-        }    
-        else if(strcmp(argv[argpos], "--interval")==0 || strcmp(argv[argpos], "-i")==0)
-        {
-            interval = atoi(argv[argpos+1]);
-            if(interval <= 0)
-                interval = 30;
-            argpos+=2;
-        }    
-        else if(strcmp(argv[argpos], "--fontsize")==0 && strcmp(argv[argpos], "-f")==0)
-        {
-            fontsize = atoi(argv[argpos+1]);
-            if(fontsize < 8)
-                fontsize = 40;
-            argpos+=2;
+            argpos += 2;
         }
-        else
-        {
+        else if (strcmp(argv[argpos], "--interval") == 0 || strcmp(argv[argpos], "-i") == 0) {
+            interval = atoi(argv[argpos + 1]);
+            if (interval <= 0)
+                interval = 30;
+            argpos += 2;
+        }
+        else if (strcmp(argv[argpos], "--fontsize") == 0 && strcmp(argv[argpos], "-f") == 0) {
+            fontsize = atoi(argv[argpos + 1]);
+            if (fontsize < 8)
+                fontsize = 40;
+            argpos += 2;
+        } else {
             printf("Error! Unknown \"%s\" parameter. Bailing out.\n", argv[argpos]);
             return -1;
-         }
-	}
-	while(1)
-	{
-		printf("Waiting %i seconds. Presss CTRL+C to quit (quick'n'dirty).\n", interval);
-		usleep(1000000*interval);
-		printf("Capturing image desktop%010i.jpg\n", counter);
-		//system("scrot desktemp.jpg");
-		system("import -window root desktemp.png");
-		time ( &rawtime );
-		timeinfo = localtime ( &rawtime );
-		sprintf(text, "convert desktemp.png -pointsize %i -stroke black -strokewidth 1 -fill white -draw \"text 50,%i '%s'\" desktop%010i.jpg", fontsize, fontsize, asctime (timeinfo), counter++);
-		system(text);
-	}
-	return 0;
+        }
+    }
+    while (1) {
+        printf("Waiting %i seconds. Presss CTRL+C to quit (quick'n'dirty).\n", interval);
+        usleep(1000000 * interval);
+        printf("Capturing image desktop%010i.jpg\n", counter);
+        //system("scrot desktemp.jpg");
+        system("import -window root desktemp.png");
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+        sprintf(text, "convert desktemp.png -pointsize %i -stroke black -strokewidth 1 -fill white -draw \"text 50,%i '%s'\" desktop%010i.jpg", fontsize, fontsize, asctime(timeinfo), counter++);
+        system(text);
+    }
+    return 0;
 }
